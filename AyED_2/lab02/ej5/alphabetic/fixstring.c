83f4b38b9b7b6c92f60df83f65b072e9da00f80d
Elcca <80895319+Elcca@users.noreply.github.com>
2023-06-09 15:33:19 -0300

    Add files via upload

---
+++ AyED_2/lab02/ej5/alphabetic/fixstring.c
@@ -0,0 +1,66 @@
+#include <stdbool.h>
+#include <assert.h>
+
+#include "fixstring.h"
+
+
+unsigned int fstring_length(fixstring s) {
+    unsigned int i = 0u;
+    while (s[i]!= '\0')
+    {
+        i++;
+    }
+    i++;
+    
+    return i;
+}
+
+bool fstring_eq(fixstring s1, fixstring s2) {
+    int i=0;
+    bool res;
+    res = fstring_length(s1)==fstring_length(s2);
+    while ((s1[i] !='\0')  && res)
+    {
+        res = res && (s1[i]==s2[i]);
+        i++;    
+    }
+    return res;
+}
+
+bool fstring_less_eq(fixstring s1, fixstring s2) {
+    int i=0;
+    bool res;
+
+    
+    while (s1[i]==s2[i] && s1[i]!='\0')
+    {
+        i++;    
+    }
+
+    if(s2[i]!='\0'){
+        res = s1[i]<s2[i];
+    } else {
+        res = false;
+    }
+    
+
+    return res;
+}
+
+void fstring_set(fixstring s1, const fixstring s2) {
+    int i=0;
+    while (i<FIXSTRING_MAX && s2[i]!='\0') {
+        s1[i] = s2[i];
+        i++;
+    }
+    s1[i] = '\0';
+}
+
+void fstring_swap(fixstring s1,  fixstring s2) {
+    fixstring aux;
+    fstring_set(aux, s2);
+    fstring_set(s2,s1);
+    fstring_set(s1,aux);
+}
+
+
